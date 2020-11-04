d1510622648efcf2ae1d05e4a715a1a64025cd27
Mykyta Solomko <sev@nix.org.ua>
2020-11-04 23:41:40 +0000

    ci fix

--- .ci/lint-yaml.sh
+++ .ci/lint-yaml.sh
@@ -6,7 +6,7 @@ CUR_DIR=$(pwd)
 YAML_FILES=$(find "${CUR_DIR}" -type f \( -iname '*.yml' -or -iname '*.yaml' \))
 
 for YM_F in ${YAML_FILES[*]}; do
-    yamllint -c "${CUR_DIR}/.drone/yamllint.yml" "${YM_F}" \
+    yamllint -c "${CUR_DIR}/.ci/yamllint.yml" "${YM_F}" \
     || RET=${?}
 done
 
